376cfdd11623d6652e75172a841b39591d4c8a68
anshriti <shanshriti1121@gmail.com>
2025-06-15 14:28:09 +0545

    django project LMS: Assignment model added and migrated

--- core/models.py
+++ core/models.py
@@ -14,7 +14,7 @@ class Student(models.Model):
     ]
     full_name = models.CharField(max_length=200, null=False, blank=False, verbose_name="Student Full Name")
     email = models.CharField(max_length=100, unique=True, null=False, blank=False, verbose_name="Student Email")
-    semester = models.Field(max_length=20, choices=SEMESTER, default='N/A', null=True, blank=True)
+    semester = models.CharField(max_length=20, choices=SEMESTER, default='N/A', null=True, blank=True)
     phone_no = models.IntegerField(null=False, blank=False)
     user =  models.OneToOneField(User, on_delete=models.CASCADE)
     class Meta:
@@ -46,3 +46,21 @@ class Teacher(models.Model):
 
     def __str__(self):
         return self.full_name
+    
+class Assignment(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Assignment Title')
+    start_date = models.DateField(default='Start Date', null=False, blank=False, verbose_name='Start Date')
+    end_date = models.DateField(default='End Date', null=False, blank=False, verbose_name='End Date')
+    question_file = models.FileField(upload_to='assignment/questions/', null=True, blank=True, verbose_name='Select Assignment File')
+    question = models.TextField(null=True, blank=True, verbose_name='Assignment Question')
+    remark = models.CharField(max_length=100, null=False, blank=False, verbose_name='Assignment Details')
+    full_mark = models.FloatField(blank=False, null=False)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name='Uploaded By')
+
+    class Meta:
+        verbose_name = 'assignment'
+        verbose_name_plural = 'assignments'
+        ordering = ['-title']
+
+    def __str__(self):
+        return self.title
